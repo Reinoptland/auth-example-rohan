8532e122197378ed189d2aaff6bfae4a72adb424
Reinoptland <reinoptland@gmail.com>
2023-02-15 17:28:16 +0100

    Move authorization to a middleware

--- index.js
+++ index.js
@@ -23,68 +23,65 @@ app.use(cors());
 app.use(cookieParser());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
+// any request, use the logger
+// app.use(logger);
 app.use(authRouter);
+
+// middleware
+function logger(req, res, next) {
+  // logging something
+  console.log(req.method, req.path);
+  // done!
+  // continue handling the request
+  next();
+}
+
+async function authMiddleWare(req, res, next) {
+  if (!req.cookies.session_token) {
+    return res.redirect("/login");
+  }
+
+  const session = await prisma.session.findFirst({
+    where: {
+      token: req.cookies["session_token"],
+    },
+  });
+
+  if (!session) {
+    return res.redirect("/login");
+  }
+
+  if (isSessionExpired(session.expiry)) {
+    res.redirect("/login");
+
+    const deletedUserToken = await prisma.session.delete({
+      where: {
+        id: session.id,
+      },
+    });
+    return console.log(deletedUserToken);
+  }
+
+  // add the session to the request
+  req.session = session;
+  // User is logged in! Session is valid!
+  next();
+}
 
 function isSessionExpired(dateObject) {
   return dateObject <= +new Date();
 }
 
-app.get("/welcome", async (req, res) => {
-  if (!req.cookies.session_token) {
-    return res.redirect("/login");
-  }
-
-  const session = await prisma.session.findFirst({
-    where: {
-      token: req.cookies["session_token"],
-    },
-  });
-
-  if (!session) {
-    return res.redirect("/login");
-  }
-
-  if (isSessionExpired(session.expiry)) {
-    res.redirect("/login");
-
-    const deletedUserToken = await prisma.session.delete({
-      where: {
-        id: session.id,
-      },
-    });
-    return console.log(deletedUserToken);
-  }
-
-  res.send(`Welcome ${session.email}!`).end();
+app.get("/welcome", authMiddleWare, async (req, res) => {
+  res.send(`Welcome ${req.session.email}!`).end();
 });
 
-app.get("/profile", async (req, res) => {
-  if (!req.cookies.session_token) {
-    return res.redirect("/login");
-  }
-
-  const session = await prisma.session.findFirst({
-    where: {
-      token: req.cookies["session_token"],
-    },
-  });
-
-  if (!session) {
-    return res.redirect("/login");
-  }
-
-  if (isSessionExpired(session.expiry)) {
-    res.redirect("/login");
-
-    const deletedUserToken = await prisma.session.delete({
-      where: {
-        id: session.id,
-      },
-    });
-    return console.log(deletedUserToken);
-  }
-
+app.get("/profile", authMiddleWare, logger, async (req, res) => {
   res.send(`Here is your profile`).end();
+});
+
+app.get("/highscores", authMiddleWare, logger, async (req, res) => {
+  res.send(`Here is your highscores`).end();
 });
 
 app.listen(PORT, () => {
